e67923a99c03cfd95c3104da6c6de05bb8e5dfed
Ayushi Gupta <mcs.ayushigupta@gmail.com>
2024-01-30 09:07:47 +0530

    commit

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,1 +1,30 @@
-const mainContainer=document.querySelector('#root')
+function customRender(reactElement, mainContainer) {
+
+    // const domElement=document.createElement(reactElement.type)
+    // domElement.innerHTML=reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // mainContainer.appendChild(domElement)
+
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    mainContainer.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+// (what to inject, where to injectj)
+customRender(reactElement, mainContainer)
