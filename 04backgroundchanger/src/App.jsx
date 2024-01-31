e7bf8d07b8f0cbe60c3a9badc65f04d5d918a10d
Ayushi Gupta <mcs.ayushigupta@gmail.com>
2024-01-31 23:24:24 +0530

    passwordGenerator

--- 04backgroundchanger/src/App.jsx
+++ 04backgroundchanger/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("#FF")
 
   return (
     <>
-      <h1>Background Changer</h1>
+    <div className="w-screen h-full bg-[#2FF] ">
+      <div className='text-3xl text-center pt-4 mb-7'>
+      Background changer
+      </div>
+      <img className='ml-4 mb-7 rounded-xl border-4' src='https://media.istockphoto.com/id/1162669873/vector/cute-panda-paws-up-over-wall-panda-face-cartoon-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=sq_TBuYnAppScO_2tdMP3AhHWTlXlHipURnI96HWObY='></img>
+      <p className="text-xl pl-4">this site changes the background color</p>
+    </div>  
     </>
   )
 }
 
 export default App
+
+
+
+
+
